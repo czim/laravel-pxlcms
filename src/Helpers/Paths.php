4db54a976d8f93d1ad9ecdef1459f7c85267893b
Coen Zimmerman <coen@pixelindustries.com>
2015-10-19 16:30:07 +0200

    tweaks for paths helper, now takes file name as parameter

--- src/Helpers/Paths.php
+++ src/Helpers/Paths.php
@@ -6,42 +6,70 @@ class Paths
     /**
      * External path to CMS images
      *
-     * @param bool $secure
+     * @param string $file      file to add to path
+     * @param bool   $secure
      * @return string
      */
-    public static function images($secure = false)
+    public static function images($file = null, $secure = false)
     {
-        return asset(config('pxlcms.paths.images'), $secure);
+        return asset(self::appendToPath(config('pxlcms.paths.images'), $file), $secure);
     }
 
     /**
      * Internal path to CMS images
      *
+     * @param string $file      file to add to path
      * @return string
      */
-    public static function imagesInternal()
+    public static function imagesInternal($file = null)
     {
-        return base_path(config('pxlcms.paths.base_internal') . '/' . config('pxlcms.paths.images'));
+        return base_path(
+            self::appendToPath(
+                self::appendToPath(config('pxlcms.paths.base_internal'), config('pxlcms.paths.images')),
+                $file
+            )
+        );
     }
 
     /**
      * External path to CMS uploads / files
      *
+     * @param string $file      file to add to path
      * @param bool $secure
      * @return string
      */
-    public static function uploads($secure = false)
+    public static function uploads($file = null, $secure = false)
     {
-        return asset(config('pxlcms.paths.files'), $secure);
+        return asset(self::appendToPath(config('pxlcms.paths.files'), $file), $secure);
     }
 
     /**
      * Internal path to CMS uploads / files
      *
+     * @param string $file      file to add to path
      * @return string
      */
-    public static function uploadsInternal()
+    public static function uploadsInternal($file = null)
     {
-        return base_path(config('pxlcms.paths.base_internal') . '/' .config('pxlcms.paths.files'));
+        return base_path(
+            self::appendToPath(
+                self::appendToPath(config('pxlcms.paths.base_internal'), config('pxlcms.paths.files')),
+                $file
+            )
+        );
+    }
+
+    /**
+     * Append a subpath or file to a path
+     *
+     * @param string $path
+     * @param string $file
+     * @return string
+     */
+    protected static function appendToPath($path, $file)
+    {
+        if (empty($file)) return $path;
+
+        return rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $file;
     }
 }
