5dccf908a0c77644d2ae80b2f1d5ac0d102cabd7
Coen Zimmerman <coen@pixelindustries.com>
2015-12-12 15:37:05 +0100

    added fix for belongsto relation name = attribute name conflicts
    now more efficiently handles eager loading

---
+++ src/Generator/Writer/Model/Steps/StubReplaceAccessorsAndMutators.php
@@ -0,0 +1,63 @@
+<?php
+namespace Czim\PxlCms\Generator\Writer\Model\Steps;
+
+use Czim\PxlCms\Generator\Generator;
+
+class StubReplaceAccessorsAndMutators extends AbstractProcessStep
+{
+
+    protected function process()
+    {
+        $this->stubPregReplace('# *{{ACCESSORS}}\n?#i', $this->getAccessorsReplace());
+    }
+
+
+    /**
+     * @return string
+     */
+    protected function getAccessorsReplace()
+    {
+
+        $accessors = [];
+
+        // for belongs-to relationships that share foreign key & relation name
+
+        foreach ($this->data['relationships']['normal'] as $name => $relationship) {
+            if ($relationship['type'] != Generator::RELATIONSHIP_BELONGS_TO) continue;
+            if ( ! empty($relationship['key']) && snake_case($name) !== $relationship['key']) continue;
+
+            $attributeName = snake_case($name);
+
+            $content = $this->tab(2) . "if (isset(\$this->relations['{$attributeName}'])) {\n"
+                     . $this->tab(3) . "return \$this->relations['{$attributeName}'];\n"
+                     . $this->tab(2) . "}\n"
+                     . $this->tab(2) . "return \$this->attributes['{$attributeName}'];\n";
+
+            $accessors[ $name ] = [
+                'content' => $content,
+            ];
+        }
+
+
+        if ( ! count($accessors)) return '';
+
+
+        $replace = "\n"
+                 . $this->tab() . "/*\n"
+                 . $this->tab() . " * Accessors & Mutators\n"
+                 . $this->tab() . " */\n\n";
+
+
+        foreach ($accessors as $name => $accessor) {
+
+            $replace .= $this->tab() . "public function get" . studly_case($name) . "Attribute()\n"
+                      . $this->tab() . "{\n"
+                      . $accessor['content']
+                      . $this->tab() . "}\n"
+                      . "\n";
+        }
+
+        return $replace;
+    }
+
+}
